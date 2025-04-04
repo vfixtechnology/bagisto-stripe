feb10aa6cd2a3a476789efd1830107d8c54f60f6
Arun chahar <info@vfixtechnology.com>
2025-04-05 01:17:50 +0530

    Initial project commit

---
+++ src/Config/paymentmethods.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'stripe'  => [
+        'code'        => 'stripe',
+        'title'       => 'Stripe',
+        'description' => 'Stripe',
+        'class'       => 'Vfixtechnology\Stripe\Payment\Stripe',
+        'active'      => true,
+        'sort'        => 7,
+    ],
+];
---
+++ src/Config/system.php
@@ -0,0 +1,50 @@
+<?php
+
+return [
+    [
+        'key'    => 'sales.payment_methods.stripe',
+        'info'   => 'Stripe extension created for bagisto by Vfixtechnology.',
+        'name'   => 'Stripe',
+        'sort'   => 7,
+        'fields' => [
+            [
+                'name'          => 'title',
+                'title'         => 'Stripe',
+                'type'          => 'text',
+                'validation'    => 'required',
+                'channel_based' => false,
+                'locale_based'  => true,
+            ], [
+                'name'          => 'description',
+                'title'         => 'Stripe Gateway',
+                'type'          => 'textarea',
+                'channel_based' => false,
+                'locale_based'  => true,
+            ],
+            [
+                'name'          => 'logo_image',
+                'title'         => 'Logo',
+                'type'          => 'image',
+                'channel_based' => false,
+                'locale_based'  => false,
+                'validation'    => 'mimes:bmp,jpeg,jpg,png,webp',
+            ],
+            [
+                'name'          => 'stripe_api_key',
+                'title'         => 'Strip Secret Key',
+                'type'          => 'text',
+                'validation'    => 'required',
+                'channel_based' => false,
+                'locale_based'  => true,
+            ],
+            [
+                'name'          => 'active',
+                'title'         => 'admin::app.configuration.index.sales.payment-methods.status',
+                'type'          => 'boolean',
+                'validation'    => 'required',
+                'channel_based' => false,
+                'locale_based'  => true,
+            ]
+        ]
+    ]
+];
